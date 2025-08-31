0e8bf699eca0456c66202f0d9154309c3500c5d9
aragon023 <mauricio.aragon@gmail.com>
2025-08-31 10:02:40 +0100

    Add logout route in app.py Update logout information in base.html

--- app.py
+++ app.py
@@ -165,7 +165,9 @@ def login():
 @login_required
 def logout():
     logout_user()
+    flash("You have been logged out successfully.", "success")
     return redirect(url_for("login"))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
